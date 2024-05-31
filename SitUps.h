38eaae67ea8ae3ae3cb9dfb13f616900d61fe1f8
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2024-05-31 00:29:29 -0600

    Update SitUps.h

--- SitUps.h
+++ SitUps.h
@@ -1,19 +1,31 @@
-# ifndef SITUPS_H_
-# define SITUPS_H_
-# include "Exercise.h"
+# include "SitUps.h"
 
-class SitUps:public Exercise{
-    private:
-        int sets;
-        int repetitions;
-    public:
-        SitUps();
-        SitUps(string, int, int);
-        int getSets();
-        void setSets(int);
-        int getRepetitions();
-        void setRepetitions(int);
-        float calculateCaloriesBurned() override;
-};
+SitUps::SitUps(){
+    sets = 0;
+    repetitions = 0;
+}
 
-# endif
+SitUps::SitUps(string _name, int _sets, int _repetitions):Exercise(_name){
+    sets = _sets;
+    repetitions = _repetitions;
+}
+
+int SitUps::getSets(){
+    return sets;
+}
+
+void SitUps::setSets(int _sets){
+    sets = _sets;
+}
+
+int SitUps::getRepetitions(){
+    return repetitions;
+}
+
+void SitUps::setRepetitions(int _repetitions){
+    repetitions = _repetitions;
+}
+
+float SitUps::calculateCaloriesBurned(){
+    return  (8 * 3.5) * sets * repetitions;
+}
