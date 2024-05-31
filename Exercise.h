07aaa2829e48c5ff799f60f91a355c33412a0d84
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2024-05-31 00:25:57 -0600

    Update Exercise.h

--- Exercise.h
+++ Exercise.h
@@ -6,14 +6,11 @@ using namespace std;
 class Exercise{
     protected:
         string name;
-        float duration;
     public:
         Exercise();
-        Exercise(string, float);
+        Exercise(string);
         string getName();
         void setName(string);
-        float getDuration();
-        void setDuration(float);
         virtual float calculateCaloriesBurned() = 0;
 };
 
