ddaab58e57f9db8360782db78832df0e7a9037b2
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2024-05-31 20:34:36 -0600

    Update PullUps.h

--- PullUps.h
+++ PullUps.h
@@ -13,7 +13,7 @@ class PullUps:public Exercise{
         void setSets(int);
         int getRepetitions();
         void setRepetitions(int);
-        float calculateCaloriesBurned() override;
+        double calculateCaloriesBurned() override;
 };
 
 # endif
