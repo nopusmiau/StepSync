a0c0fa8ce1fa469a29c509f7f16e91d26481f920
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2024-06-07 07:39:43 -0600

    Update Routine.cpp

--- Routine.cpp
+++ Routine.cpp
@@ -26,9 +26,9 @@ string Routine::getRoutineInfo(){
             Running *p1 = (Running *) exercises[j];
             detailedContent = detailedContent + p1->getName() + "\n";
             running_1 = running_1 + p1->getDistance(" km") + "\n";
-            running_2 = running_2 + p1 ->getDuration(" minutes") + "\n";
+            running_2 = running_2 + p1->getDuration(" minutes") + "\n";
             detailedContent = detailedContent + running_1 + running_2;
-            detailedContent = detailedContent +  "Calories Burned: " + to_string(p1->calculateCaloriesBurned()) + "\n";
+            detailedContent = detailedContent +  "Calories Burned (without weight): " + to_string(p1->calculateCaloriesBurned()) + "\n";
         }
         else if(type_info == typeid(PushUps)){
             PushUps *p1 = (PushUps *) exercises[j];
@@ -36,7 +36,7 @@ string Routine::getRoutineInfo(){
             msg_1 = msg_1 + to_string(p1->getSets()) + "\n";
             msg_2 = msg_2 + to_string(p1->getRepetitions()) + "\n";
             detailedContent = detailedContent + msg_1 + msg_2;
-            detailedContent = detailedContent + "Calories Burned: " + to_string(p1->calculateCaloriesBurned()) + "\n";
+            detailedContent = detailedContent + "Calories Burned (without weight): " + to_string(p1->calculateCaloriesBurned()) + "\n";
         }
         else if(type_info == typeid(PullUps)){
             PullUps *p1 = (PullUps *) exercises[j];
@@ -44,7 +44,7 @@ string Routine::getRoutineInfo(){
             msg_1 = msg_1 + to_string(p1->getSets()) + "\n";
             msg_2 = msg_2 + to_string(p1->getRepetitions()) + "\n";
             detailedContent = detailedContent + msg_1 + msg_2;
-            detailedContent = detailedContent + "Calories Burned: " + to_string(p1->calculateCaloriesBurned()) + "\n";
+            detailedContent = detailedContent + "Calories Burned (without weight): " + to_string(p1->calculateCaloriesBurned()) + "\n";
         }
         else if(type_info == typeid(SitUps)){
             SitUps *p1 = (SitUps *) exercises[j];
@@ -52,7 +52,58 @@ string Routine::getRoutineInfo(){
             msg_1 = msg_1 + to_string(p1->getSets()) + "\n";
             msg_2 = msg_2 + to_string(p1->getRepetitions()) + "\n";
             detailedContent = detailedContent + msg_1 + msg_2;
-            detailedContent = detailedContent + "Calories Burned: " + to_string(p1->calculateCaloriesBurned()) + "\n";
+            detailedContent = detailedContent + "Calories Burned (without weight): " + to_string(p1->calculateCaloriesBurned()) + "\n";
+        }
+        else{
+            detailedContent = detailedContent;
+        }
+        content = content + detailedContent + "\n";
+    }
+    content = content + "-----------------------";
+    return content;
+}
+
+string Routine::getRoutineInfoWithWeight(int weight){
+    string content = "-----------------------\n";
+    for(int j = 0; j < exercises.size(); ++j){
+
+        string detailedContent = "";
+        string running_1 = "Distance: ";
+        string running_2 = "Duration: ";
+        string msg_1 = "Sets: ";
+        string msg_2 = "Repetitions: ";
+        const type_info& type_info = typeid(*exercises[j]);
+        if(type_info == typeid(Running)){
+            Running *p1 = (Running *) exercises[j];
+            detailedContent = detailedContent + p1->getName() + "\n";
+            running_1 = running_1 + p1->getDistance(" km") + "\n";
+            running_2 = running_2 + p1 ->getDuration(" minutes") + "\n";
+            detailedContent = detailedContent + running_1 + running_2;
+            detailedContent = detailedContent +  "Calories Burned (with weight): " + to_string(p1->calculateCaloriesBurned(weight)) + "\n";
+        }
+        else if(type_info == typeid(PushUps)){
+            PushUps *p1 = (PushUps *) exercises[j];
+            detailedContent = detailedContent + p1->getName() + "\n";
+            msg_1 = msg_1 + to_string(p1->getSets()) + "\n";
+            msg_2 = msg_2 + to_string(p1->getRepetitions()) + "\n";
+            detailedContent = detailedContent + msg_1 + msg_2;
+            detailedContent = detailedContent + "Calories Burned (with weight): " + to_string(p1->calculateCaloriesBurned(weight)) + "\n";
+        }
+        else if(type_info == typeid(PullUps)){
+            PullUps *p1 = (PullUps *) exercises[j];
+            detailedContent = detailedContent + p1->getName() + "\n";
+            msg_1 = msg_1 + to_string(p1->getSets()) + "\n";
+            msg_2 = msg_2 + to_string(p1->getRepetitions()) + "\n";
+            detailedContent = detailedContent + msg_1 + msg_2;
+            detailedContent = detailedContent + "Calories Burned (with weight): " + to_string(p1->calculateCaloriesBurned(weight)) + "\n";
+        }
+        else if(type_info == typeid(SitUps)){
+            SitUps *p1 = (SitUps *) exercises[j];
+            detailedContent = detailedContent + p1->getName() + "\n";
+            msg_1 = msg_1 + to_string(p1->getSets()) + "\n";
+            msg_2 = msg_2 + to_string(p1->getRepetitions()) + "\n";
+            detailedContent = detailedContent + msg_1 + msg_2;
+            detailedContent = detailedContent + "Calories Burned (with weight): " + to_string(p1->calculateCaloriesBurned(weight)) + "\n";
         }
         else{
             detailedContent = detailedContent;
